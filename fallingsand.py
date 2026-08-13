b35cf106ce4bf36dec760adee20a49dd1396deba
Haziq J <yepmcc@gmail.com>
2026-08-12 22:33:18 -0400

    Very simply worked on display features

--- fallingsand.py
+++ fallingsand.py
@@ -5,6 +5,8 @@ pygame.init()
 
 WINDOW_WIDTH = 800 
 WINDOW_HEIGHT = 600
+FPS = 120
+GREY = (29,29,29)
 
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Falling Sand Sim")
@@ -26,3 +28,7 @@ while True:
 
 
     #Drawing Grid
+    window.fill(GREY)
+    
+    pygame.display.flip()
+    clock.tick(FPS)
