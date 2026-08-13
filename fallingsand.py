028debcbc5eb34e89e985794a618c50bc8b273fa
Haziq J <yepmcc@gmail.com>
2026-08-12 22:20:27 -0400

    Added some generale layouting

--- fallingsand.py
+++ fallingsand.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 
 pygame.init()
@@ -10,4 +10,19 @@ window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Falling Sand Sim")
 
 clock = pygame.time.Clock()
- 
+
+#Simulation Loop
+
+while True: 
+    
+    #Event Handling:
+    for event in pygame.event.get(): #This line of code gets all the events that pygame gets since the last time the while loop ran, and puts them in a list
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Updating State
+
+
+
+    #Drawing Grid
